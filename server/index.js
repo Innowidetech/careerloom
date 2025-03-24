53f2c4c26529fa5c89dfc470280307daa4957f6b
Innowidetech <innowidetech@gmail.com>
2025-03-24 16:04:06 +0530

    server

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:["http://localhost:5173","https://jobportal-03.web.app"],
+    origin:["http://localhost:5173","https://jobportal-03.web.app", "https://careerloom-i.web.app"],
     credentials:true
 }
 
